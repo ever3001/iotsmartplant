3855597d2cce9181dd697838835fc038f159c63e
Ever ATILANO <ever.developer3001@gmail.com>
2022-04-06 11:18:27 +0200

    [REFACT] lib: Semaphores: Remplaced Queues for shared memory. Added Initial files

---
+++ lib/Semaphores/Semaphores.c
@@ -0,0 +1,23 @@
+#include "Semaphores.h"
+#include "esp_log.h"
+#include "Moisture_Sensor.h"
+#include "Water_Sensor.h"
+#include "DHT22.h"
+#include "MQTT.h"
+
+esp_err_t setup_semaphores(){
+  esp_err_t err = ESP_FAIL;
+  ESP_LOGI("[SEMAPHORES]", "Setting up semaphores...\r\n");
+  _moisture_sensor_data.xSemaphore = xSemaphoreCreateMutex();
+  if(_moisture_sensor_data.xSemaphore == NULL) { goto error; }
+  _water_sensor_data.xSemaphore = xSemaphoreCreateMutex();
+  if(_water_sensor_data.xSemaphore == NULL) { goto error; }
+  _DHT22_data.xSemaphore = xSemaphoreCreateMutex();
+  if(_DHT22_data.xSemaphore == NULL) { goto error; }
+  _mqtt.xSemaphore = xSemaphoreCreateMutex();
+  if(_mqtt.xSemaphore == NULL) { goto error; }
+  ESP_LOGI("[SEMAPHORES]", "Semaphores created\r\n");
+  err = ESP_OK;
+error:
+  return err;
+}
