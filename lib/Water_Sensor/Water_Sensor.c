ee69e057313b2b616626a90f33473a8b3fcc86b3
Ever ATILANO <ever.developer3001@gmail.com>
2022-04-06 11:25:51 +0200

    [REFACT] lib: Water_Sensor: Initial files

--- lib/Water_Sensor/Water_Sensor.c
+++ lib/Water_Sensor/Water_Sensor.c
@@ -1,65 +1,47 @@
 #include "Water_Sensor.h"
-/* Espressif Headers */
-#include "driver/adc.h"
 #include "esp_log.h"
 
-/* FreeRTOS Headers */
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-
-#define WATER_SENSOR_ADC_CHANNEL       (ADC1_CHANNEL_7)  // GPIO34
-#define NO_OF_SAMPLES                  (64)              // Multisampling
-#define TAG                            ("WATER_SEN")
-#define WATER_CHECK_INTERVAL_IN_SEC    (15)
-
-uint32_t water_val = 0;
-
-uint32_t getWaterVal(void)             { return water_val; }
-static void setWaterVal(uint32_t val)  { water_val = val; }
-
-static esp_err_t init_sensor()
-{
-  ESP_LOGI(TAG, "Initializing Water Sensor...");
-  setWaterVal(0);
-  esp_err_t esp_error = ESP_FAIL;
-  /************** Configure ADC */
-  // Initialize ADC1 with a 12 bit resolution
-  esp_error = adc1_config_width(ADC_WIDTH_BIT_12);
-  if(esp_error != ESP_OK) { goto error_01; }
-  // Initialize the channel 7 full-scale between 150 to 2450 mV
-  esp_error = adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
-  if(esp_error != ESP_OK) { goto error_02; }
-  ESP_LOGI(TAG, "Water Sensor Initialized");
-  ESP_ERROR_CHECK(esp_error);
-  return ESP_OK;
-error_02:
-  ESP_LOGI(TAG, "Deinit ADC1 channel 7 for water sensor");
-  // TODO: Deinit ADC1 channel 7
-error_01:
-  ESP_LOGI(TAG, "Disconfigure ADC1 widht");
-  // TODO: Deconfig ADC1 width
-  ESP_LOGE(TAG, "Error in Water Sensor");
-  return esp_error;
+esp_err_t setup_water_sensor(){
+    esp_err_t esp_error = ESP_FAIL;
+    ESP_LOGI("[Water_sensor]", "Setting up Water_sensor...\r\n");
+    // Initialize ADC1 with a 12 bit resolution
+    esp_error = adc1_config_width(ADC_WIDTH_BIT_12);
+    if(esp_error != ESP_OK) { goto error; }
+    // Initialize the channel 7 full-scale between 150 to 2450 mV
+    esp_error = adc1_config_channel_atten(CONFIG_ADC_WATER_SENSOR_CH, ADC_ATTEN_DB_11);
+    if(esp_error != ESP_OK) { goto error; }
+    ESP_LOGI("[Water_sensor]", "Water_sensor setup complete\r\n");
+error:
+    return esp_error;
 }
 
-void water_sensor_task(void* pvParameter)
-{
-  // Save the value of the raw adc
-  uint32_t adc_reading = 0;
-  // Calculate the task delay
-  const TickType_t xDelay = pdMS_TO_TICKS(WATER_CHECK_INTERVAL_IN_SEC * 1000);
-  for(;;) {
-    adc_reading = 0;
-    // Multisampling
-    for(size_t i = 0; i < NO_OF_SAMPLES; ++i) {
-      adc_reading += adc1_get_raw(WATER_SENSOR_ADC_CHANNEL);
-    }
-    adc_reading /= NO_OF_SAMPLES;
-    setWaterVal(adc_reading);
-    ESP_LOGI(TAG, "Water Sensor value = %d", getWaterVal());
-    // Block task for time calculated
-    vTaskDelay(xDelay);
+void water_sensor_task(void* pvParameter){
+  if( _water_sensor_data.xSemaphore != NULL ){
+        const TickType_t xDelay = pdMS_TO_TICKS(CONFIG_ADC_WATER_SENSOR_TASK_TIME_IN_SEC * 1000);
+        int adc_reading;
+        for(;;) {
+            adc_reading = 0;
+#ifdef CONFIG_ADC_WATER_SENSOR_MULTISAMPLE
+            // Multisampling
+            for(size_t i = 0; i < CONFIG_ADC_WATER_SENSOR_MULTISAMPLE_NO_SAMPLES; ++i) {
+                adc_reading += adc1_get_raw(CONFIG_ADC_WATER_SENSOR_CH);
+            }
+            adc_reading /= CONFIG_ADC_WATER_SENSOR_MULTISAMPLE_NO_SAMPLES;
+#else
+            // Single sampling
+            adc_reading = adc1_get_raw(CONFIG_ADC_WATER_SENSOR_CH);
+#endif
+            // save the value
+            xSemaphoreTake(_water_sensor_data.xSemaphore, portMAX_DELAY);
+            _water_sensor_data.value = adc_reading;
+            // print the value
+            ESP_LOGI("[Water_sensor]", "Water_sensor value: %d", _water_sensor_data.value);
+            xSemaphoreGive(_water_sensor_data.xSemaphore);
+            // wait for the next cycle
+            vTaskDelay(xDelay);
+        }
+  }else{
+        ESP_LOGE("[Water_sensor]", "Semaphore not initialized\r\n");
   }
   vTaskDelete(NULL);
 }
